7e0234a76ab53946e45fc68fb6e2a7e726cb2a0e
jukari <jukari98@gmail.com>
2021-02-05 13:11:24 -0600

    Solo falta la pagina para administrar los provedores de la pagina web

--- src/app/pages/agregar/agregar.component.ts
+++ src/app/pages/agregar/agregar.component.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarComponent implements OnInit {
 
-  constructor() { }
+  url = '../../../assets/upload2.png';
+  id: number;
+
+  constructor() {
+
+    this.generadorId();
+
+   }
 
   ngOnInit(): void {
+  }
+
+  onSelectFile(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      reader.onload = (event) => { // called once readAsDataURL is completed
+        this.url = event.target.result as string;
+      }
+    }
+  }
+
+  generadorId(){
+    let temp:string, rand:number ;
+
+    temp = '';
+
+    for(let k = 0; k < 5; k++){
+      rand = Math.random() * 9 | 0;
+      temp += rand;
+    }
+
+    this.id = parseInt(temp);
+
   }
 
 }
